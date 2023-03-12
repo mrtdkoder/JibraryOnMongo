0f4c118caf6e7eaef5f8282fffcbb8ff8a70a155
mrtdkoder <muratti21@hotmail.com>
2023-03-12 22:17:51 +0000

    some unnecessary lines removed

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,8 +7,6 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class main {
-    public static Scanner input = new Scanner(System.in);
-    public static BookRepostory br = new BookRepostory(true);
     public static void main(String[] args) {
 
         HomeView homeView = new HomeView();
